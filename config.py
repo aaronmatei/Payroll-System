deae6fa2d087ca92bccef3a5e73901f6d709e36d
aaronmatei <aaronmatei>
2019-08-03 17:18:57 +0300

    heroku deployment

--- config.py
+++ config.py
@@ -15,4 +15,5 @@ class Testing (Config):
 class Production (Config):
     SQLALCHEMY_DATABASE_URI = 'postgres://vsrhdeqzlaautl:a7fdfc1bb3f98349e8b0609e96b5141ab5dfb0daafa7a30fa3c38a174d831aff@ec2-54-221-215-228.compute-1.amazonaws.com:5432/d53fueuquhq8e1'
     DEBUG = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     environment = 'Production'
